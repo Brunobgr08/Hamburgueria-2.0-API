8fb13c36d17359d713efe9fd9b8288c3d813db91
Bruno Guedes Rodrigues <brunobgr0810@gmail.com>
2022-01-19 02:40:53 -0300

    feat: iniciando api json-server hamburgueria2.0

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.db = router.db;
 
 const rules = auth.rewriter({
   users: 600,
+  products: 644,
+  cart: 660,
 });
 
 app.use(cors());
@@ -18,6 +20,4 @@ app.use(auth);
 app.use(router);
 app.listen(port);
 
-console.log("Server is running on port:", port);
-
-/* A senha do Kenzinho é 123456 */
+console.log("O servidor está rodando na porta:", port);
